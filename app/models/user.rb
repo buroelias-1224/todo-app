89f92d78ed50c395228ca84c03d1e2f6b0ce37ad
Bro E. Ibrahim <buroelias4@gmail.com>
2025-05-30 18:20:57 +0300

    Remove devise from User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # Remove or comment out the devise line below if present:
+  # devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 end
